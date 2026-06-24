44cfd08bb447c996e1fbd851e4b27e23f4533150
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2026-06-24 23:15:31 +0200

    attempt opta

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,155 @@
 #include <Arduino.h>
-#include <WebServer.h>
 #include <LittleFS.h>
 #include <SPI.h>
-#include <wifi.h>
+#include <Ethernet.h>
 
+// ===== SERVER =====
+EthernetServer server(80);
 
+// ===== STATE =====
+int pwmVal[4] = {0, 0, 0, 0};
 
+// ===== PINS (adjust for Opta!) =====
+const int pwmPins[4] = {3, 5, 6, 9}; // NOTE: change to real Opta PWM-capable pins
 
-WebServer server(80);
-
-// PWM config
-const int pwmPins[4] = {16, 17, 18, 19};
-const int pwmCh[4]   = {0, 1, 2, 3};
-int pwmVal[4] = {0,0,0,0};
-
+// ===== SETUP =====
 void setup() {
   Serial.begin(115200);
-  Serial.println("started");
+  Serial.println("Opta starting...");
 
+  // ===== Ethernet =====
+  byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+  Ethernet.begin(mac);
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED){
-delay(500);
-Serial.print('.');
-  } 
+  delay(1000);
+  Serial.print("IP: ");
+  Serial.println(Ethernet.localIP());
 
-  Serial.println(WiFi.localIP());
+  server.begin();
 
-  // Start filesystem
+  // ===== FS =====
   if (!LittleFS.begin()) {
     Serial.println("LittleFS mount failed");
-    return;
   }
 
-  // PWM setup
+  // ===== PWM setup (Opta-safe) =====
   for (int i = 0; i < 4; i++) {
-    ledcSetup(pwmCh[i], 5000, 8);
-    ledcAttachPin(pwmPins[i], pwmCh[i]);
+    pinMode(pwmPins[i], OUTPUT);
+    analogWrite(pwmPins[i], 0);
+  }
+}
+
+// ===== LOOP =====
+void loop() {
+  EthernetClient client = server.available();
+  if (!client) return;
+
+  String req = "";
+  bool blank = false;
+
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+      req += c;
+
+      if (c == '\n' && blank) break;
+      blank = (c == '\n');
+    }
   }
 
+  // =========================
+  // ROUTING (manual parsing)
+  // =========================
 
-  server.on("/", HTTP_GET, []() {
+  // ===== INDEX =====
+  if (req.indexOf("GET / ") >= 0) {
     File file = LittleFS.open("/index.html", "r");
-    server.streamFile(file, "text/html");
+
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println();
+
+    while (file.available()) client.write(file.read());
     file.close();
-  });
-  server.on("/chart.js", HTTP_GET, []() {
-  File file = LittleFS.open("/chart.js", "r");
-  server.streamFile(file, "application/javascript");
-  file.close();
-});
+  }
 
-  server.on("/logo.gif", HTTP_GET, []() {
-  File file = LittleFS.open("/logo.gif", "r");
-  server.streamFile(file, "image/gif");
-  file.close();
-});
-server.on("/api/gpio", HTTP_GET, []() {
+  // ===== CHART JS =====
+  else if (req.indexOf("GET /chart.js") >= 0) {
+    File file = LittleFS.open("/chart.js", "r");
 
-  int pin = server.arg("pin").toInt();
-  int state = server.arg("state").toInt();
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: application/javascript");
+    client.println();
 
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, state);
+    while (file.available()) client.write(file.read());
+    file.close();
+  }
 
-  server.send(200, "application/json", "{\"ok\":true}");
-});
+  // ===== GIF =====
+  else if (req.indexOf("GET /logo.gif") >= 0) {
+    File file = LittleFS.open("/logo.gif", "r");
 
-server.on("/api/adc", HTTP_GET, []() {
-  int adcValue = random(0, 4095);
-  server.send(200, "application/json", "{\"value\":" + String(adcValue) + "}");
-});
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: image/gif");
+    client.println();
 
-  server.on("/api/pwm", HTTP_GET, []() {
-    int ch = server.arg("ch").toInt();
-    int val = server.arg("val").toInt();
+    while (file.available()) client.write(file.read());
+    file.close();
+  }
+
+  // ===== GPIO API =====
+  else if (req.indexOf("GET /api/gpio") >= 0) {
+
+    int pinIndex = req.indexOf("pin=");
+    int stateIndex = req.indexOf("state=");
+
+    int pin = req.substring(pinIndex + 4, pinIndex + 6).toInt();
+    int state = req.substring(stateIndex + 6, stateIndex + 7).toInt();
+
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, state);
+
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: application/json");
+    client.println();
+    client.println("{\"ok\":true}");
+  }
+
+  // ===== ADC API =====
+  else if (req.indexOf("GET /api/adc") >= 0) {
+
+    int adcValue = random(0, 4095);
+
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: application/json");
+    client.println();
+
+    client.print("{\"value\":");
+    client.print(adcValue);
+    client.println("}");
+  }
+
+  // ===== PWM API =====
+  else if (req.indexOf("GET /api/pwm") >= 0) {
+
+    int chIndex = req.indexOf("ch=");
+    int valIndex = req.indexOf("val=");
+
+    int ch = req.substring(chIndex + 3, chIndex + 4).toInt();
+    int val = req.substring(valIndex + 4).toInt();
 
     val = constrain(val, 0, 255);
 
     if (ch >= 0 && ch < 4) {
       pwmVal[ch] = val;
-      ledcWrite(pwmCh[ch], val);
+      analogWrite(pwmPins[ch], val);
     }
 
-    server.send(200, "application/json", "{\"ok\":true}");
-  });
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: application/json");
+    client.println();
+    client.println("{\"ok\":true}");
+  }
 
-  server.begin();
-}
-
-void loop() {
-  server.handleClient();
+  client.stop();
 }
